498cd09d6fb2f0adf2d6c8d7a9e0b8d79492593d
Vinh <vinhvt@robot.co.jp>
2018-04-22 09:39:17 +0700

    Add development scene

--- Assets/App/Scripts/Installers/GameInstaller.cs
+++ Assets/App/Scripts/Installers/GameInstaller.cs
@@ -15,17 +15,27 @@ public class GameInstaller : MonoInstaller<GameInstaller>
         Container.BindFactory<int, int, int, CellData, CellData.Factory>().FromNew();
         Container.BindFactoryContract<ICell, IFactory<ICell>, Cell.Factory>()
             .FromComponentInNewPrefab(settings.cellPrefab);
-        
+
         Container.Bind<IList<ICell>>().FromInstance(new List<ICell>()).AsSingle();
         Container.Bind<IList<CellData>>().FromInstance(new List<CellData>()).AsSingle();
         Container.Bind<IReactiveProperty<GameStatus>>().FromInstance(new ReactiveProperty<GameStatus>()).AsSingle();
-        
+
         Container.Bind<Random>().FromMethod(_ => new Random()).AsTransient();
         Container.Bind<IntReactiveProperty>().FromMethod(_ => new IntReactiveProperty()).AsTransient();
         Container.Bind<BoolReactiveProperty>().FromMethod(_ => new BoolReactiveProperty()).AsTransient();
-        
+
         Container.Bind<IGameBoard>().To<GameBoard>().AsSingle().NonLazy();
         Container.Bind<IGameSolver>().To<GameSolver>().AsSingle().NonLazy();
-        Container.Bind<MapGenerator>().FromComponentInNewPrefab(settings.canvasPrefab).AsSingle().NonLazy();
+
+        var mapGenerator = FindObjectOfType<MapGenerator>();
+
+        if (mapGenerator)
+        {
+            Container.BindInstance(mapGenerator).AsSingle();
+        }
+        else
+        {
+            Container.Bind<MapGenerator>().FromComponentInNewPrefab(settings.canvasPrefab).AsSingle().NonLazy();
+        }
     }
 }
